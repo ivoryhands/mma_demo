410fff64c0b34c7260d740e43b9991dfce4bf83c
IVORYHANDS <egapage@gmail.com>
2017-01-25 16:18:41 -0700

    home page coded to hide sign in/up buttons if not authenticated

--- src/app/components/home/Home.jsx
+++ src/app/components/home/Home.jsx
@@ -1,23 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { connect } from 'react-redux';
 
-function Home() {
-  return (
-    <div>
-    <section className="intro bg-cover">
-      <div className="content">
-        <h1 className="white-text">ClashMMA</h1>
-        <p className="white-text">Clash against other fight fans as you make fight picks in realtime and compete for top spot.</p>
-        <Link to ="signin" className="blocks" role="button">Sign In</Link>
-        <Link to ="signup" className="blocks" role="button">Sign Up</Link>
-
-     </div>
-    </section>
-    <footer>
-        <span>Made by EGAP</span>
-    </footer>
-  </div>
-  )
+class Home extends Component {
+  constructor (props) {
+    super(props);
+    console.log(this.props, 'Home props');
+  }
+  showButtons() {
+    if (this.props.authenticated) {
+      return <div></div>
+    }
+    else {
+      return  <div>
+                <Link to ="signup" className="blocks" role="button">Sign Up</Link>
+                <Link to ="signin" className="blocks" role="button">Sign In</Link>
+              </div>
+    }
+  }
+  render() {
+    return (
+      <div>
+          <section className="intro bg-cover">
+            <div className="content">
+              <h1 className="white-text">ClashMMA</h1>
+              <p className="white-text">Clash against other fight fans as you make fight picks in realtime and compete for top spot.</p>
+              {this.showButtons()}
+           </div>
+          </section>
+          <footer>
+              <span>Made by EGAP</span>
+          </footer>
+        </div>
+      );
+    }
 }
 
-export default Home;
+function mapStateToProps (state) {
+  return {
+    email: state.user.email,
+    displayName: state.user.displayName,
+    photoURL: state.user.photoURL,
+    uid: state.user.uid,
+    authCompleted: state.auth.authCompleted,
+    authenticated: state.auth.authenticated
+  }
+}
+
+export default Home = connect(mapStateToProps)(Home);
